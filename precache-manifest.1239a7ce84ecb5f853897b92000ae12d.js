0e7477b1dc9b51d0399adec6b0f208ede41e2684
Nguyen <DNguyen@ECFMG.ORG>
2020-06-09 21:48:54 -0400

    Updates

---
+++ precache-manifest.1239a7ce84ecb5f853897b92000ae12d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8aa2f4bc9d46755428a212fe3c6cda0",
+    "url": "/helen-learn-math/index.html"
+  },
+  {
+    "revision": "db2e92ff33f01fd6e4de",
+    "url": "/helen-learn-math/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "e2e75288df8715ae6e63",
+    "url": "/helen-learn-math/static/css/main.0bfb6109.chunk.css"
+  },
+  {
+    "revision": "db2e92ff33f01fd6e4de",
+    "url": "/helen-learn-math/static/js/2.1b0ff34c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/helen-learn-math/static/js/2.1b0ff34c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e75288df8715ae6e63",
+    "url": "/helen-learn-math/static/js/main.0be37e7d.chunk.js"
+  },
+  {
+    "revision": "f85f81c584c1e00bf893",
+    "url": "/helen-learn-math/static/js/runtime-main.f4430506.js"
+  }
+]);
